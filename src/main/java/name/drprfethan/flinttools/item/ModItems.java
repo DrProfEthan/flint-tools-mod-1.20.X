164b432c3551d404dd672135598d9662c8cc12e3
Ethan Summers <ethan200019@gmail.com>
2023-08-17 17:18:02 -0700

    Tool Modifiers and Recipes

--- src/main/java/name/drprfethan/flinttools/item/ModItems.java
+++ src/main/java/name/drprfethan/flinttools/item/ModItems.java
@@ -4,19 +4,28 @@ import name.drprfethan.flinttools.FlintTools;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item FLINT_AXE = registerItem("flint_axe", new Item(new FabricItemSettings()));
-    public static final Item FLINT_HOE = registerItem("flint_hoe", new Item(new FabricItemSettings()));
-    public static final Item FLINT_PICKAXE = registerItem("flint_pickaxe", new Item(new FabricItemSettings()));
-    public static final Item FLINT_SHOVEL = registerItem("flint_shovel", new Item(new FabricItemSettings()));
-    public static final Item FLINT_SWORD = registerItem("flint_sword", new Item(new FabricItemSettings()));
+    public static final Item FLINT_AXE = registerItem("flint_axe",
+            new AxeItem(ModToolMaterials.FLINT, 6, -3.2f,
+            new FabricItemSettings()));
+    public static final Item FLINT_HOE = registerItem("flint_hoe",
+            new HoeItem(ModToolMaterials.FLINT, 0, -3.0f,
+                    new FabricItemSettings()));
+    public static final Item FLINT_PICKAXE = registerItem("flint_pickaxe",
+            new PickaxeItem(ModToolMaterials.FLINT, 1, -2.8f,
+                    new FabricItemSettings()));
+    public static final Item FLINT_SHOVEL = registerItem("flint_shovel",
+            new ShovelItem(ModToolMaterials.FLINT, 2, -3.0f,
+                    new FabricItemSettings()));
+    public static final Item FLINT_SWORD = registerItem("flint_sword",
+            new SwordItem(ModToolMaterials.FLINT, 3, -2.4f,
+                    new FabricItemSettings()));
 
     private static void addItemsToToolsItemGroup(FabricItemGroupEntries entries) {
         entries.add(FLINT_AXE);
